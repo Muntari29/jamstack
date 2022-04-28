85b956b797fb85182194e37c040f6b9311a991be
Muntari29 <muntrock@gmail.com>
2022-04-28 16:20:25 +0900

    feat: 무한스크롤 기능 구현

--- src/hooks/useInfiniteScroll.tsx
+++ src/hooks/useInfiniteScroll.tsx
@@ -1,4 +1,4 @@
-import { MutableRefObject, useState, useRef, useMemo } from 'react'
+import { MutableRefObject, useState, useEffect, useRef, useMemo } from 'react'
 import { PostListItemType } from 'types/PostItem.types'
 
 export type useInfiniteScrollType = {
@@ -30,6 +30,33 @@ const useInfiniteScroll = function (
       ),
     [selectedCategory],
   )
+
+  const observer: IntersectionObserver = new IntersectionObserver(
+    (entries, observer) => {
+      //배열 내의 데이터에는 isIntersecting이라는 프로퍼티를 통해 화면에 노출되었는지를 확인
+      if (!entries[0].isIntersecting) return
+
+      setCount(value => value + 1)
+      observer.disconnect()
+    },
+  )
+
+  //선택된 카테고리가 변경된 경우에는 count 값을 1로 변경해주는 코드
+  useEffect(() => setCount(1), [selectedCategory])
+
+  useEffect(() => {
+    if (
+      NUMBER_OF_ITEMS_PER_PAGE * count >= postListByCategory.length ||
+      containerRef.current === null ||
+      containerRef.current.children.length === 0
+    )
+      return
+
+    observer.observe(
+      containerRef.current.children[containerRef.current.children.length - 1],
+    )
+    //count 값과 선택된 카테고리가 값이 변경될 때마다 관측 요소 변경
+  }, [count, selectedCategory])
 
   return {
     containerRef,
